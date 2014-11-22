957b2d77c9bd1dc92ab05337d9936dfeff417121
powerrmt <powerrmt@gmail.com>
2014-11-22 16:58:25 -0500

    Moved attack component of strategy into a method, since we need to call it in two places; fixed bug where the formation would remain stationary for an unneccessary turn

--- src/outpost/group3/ConsumerStrategy.java
+++ src/outpost/group3/ConsumerStrategy.java
@@ -8,8 +8,12 @@ public class ConsumerStrategy extends outpost.group3.Strategy {
   private int size = 100;
   enum State { BUILD, ATTACK };
   static State state = State.BUILD;
-  
-  
+
+  //These will represent our current outpost locations
+  ArrayList<Loc> outposts = new ArrayList<Loc>();
+
+  //center of our consumer at the start
+  Loc consCenter = new Loc(); 
 
   ConsumerStrategy() {}
 
@@ -20,10 +24,11 @@ public class ConsumerStrategy extends outpost.group3.Strategy {
   public ArrayList<Loc> run(Board board) {
     ArrayList<Loc> targets = new ArrayList<Loc>();
 
-    ArrayList<Loc> outposts = board.ourOutposts();
+    outposts = board.ourOutposts();
     int numOutposts = outposts.size();
 
-    Loc consCenter = new Loc(); //center of our consumer at the start
+
+    //These represent the positions of the 4 outposts composing a consumer
     Loc north = new Loc();
     Loc east = new Loc();
     Loc south = new Loc();
@@ -69,51 +74,26 @@ public class ConsumerStrategy extends outpost.group3.Strategy {
             if (west.equals(outpost))
               numInPosition++;
           }
-          System.out.println(numInPosition);
 
           //We have fully formed a consumer
           if(numInPosition >= 4){
             state = State.ATTACK;
+            attackMove(targets,board);
           }
+          else{
+            targets.add(north);
+            targets.add(south);
+            targets.add(east);
+            targets.add(west);
+          }
+
           break;
 
         case ATTACK: 
-
-          //Find closest enemy
-
-          double enemyDist = 100;
-
-          Loc centerTarget = new Loc();
-          ArrayList<Loc> enemyOutposts = new ArrayList<Loc>();
-
-          for (int i = 0; i < 4; i ++){
-            enemyOutposts = board.theirOutposts(i);
-            if (enemyOutposts == outposts)
-              continue;
-            for (Loc outpost : enemyOutposts){
-              double tempDist = outpost.distance(consCenter);
-              if (tempDist < enemyDist){
-                enemyDist = tempDist;
-                centerTarget = outpost;
-              }
-            }
-          }
-          System.out.println("Moving to target " + centerTarget);
-
-          north = board.nearestLand(new Loc(centerTarget.x,centerTarget.y + r));
-          south = board.nearestLand(new Loc(centerTarget.x,centerTarget.y - r));
-          east = board.nearestLand(new Loc(centerTarget.x + r,centerTarget.y));
-          west = board.nearestLand(new Loc(centerTarget.x - r,centerTarget.y));
-
+          attackMove(targets,board);
           break;
 
-        
       }
-
-      targets.add(north);
-      targets.add(south);
-      targets.add(east);
-      targets.add(west);
 
       for (int i = 4; i < outposts.size(); i++){
         targets.add(outposts.get(i));
@@ -122,15 +102,49 @@ public class ConsumerStrategy extends outpost.group3.Strategy {
     }
     else{
       double sideLength = Math.min(65, Math.sqrt(Math.pow(2*board.r*numOutposts * 1, 2) / 2));
-              
+
       for (int outpostId = 0; outpostId < numOutposts; outpostId++) {
-              int xTarget = (int) Math.round((outpostId + 1) * (sideLength / (numOutposts + 1)));
-              int yTarget = (int) Math.round((numOutposts - outpostId) * (sideLength / (numOutposts + 1)));
-              
-              targets.add(board.nearestLand(new Loc(xTarget, yTarget)));
+        int xTarget = (int) Math.round((outpostId + 1) * (sideLength / (numOutposts + 1)));
+        int yTarget = (int) Math.round((numOutposts - outpostId) * (sideLength / (numOutposts + 1)));
+
+        targets.add(board.nearestLand(new Loc(xTarget, yTarget)));
       }
     }
-      return targets;
+    return targets;
+  }
+
+  //Set the next move for our consumer formation to attack the closest enemy
+  private void attackMove(ArrayList<Loc> targets, Board board){
+    //Find closest enemy
+
+    double enemyDist = 100;
+
+    Loc centerTarget = new Loc();
+    ArrayList<Loc> enemyOutposts = new ArrayList<Loc>();
+
+    for (int i = 0; i < 4; i ++){
+      enemyOutposts = board.theirOutposts(i);
+      if (enemyOutposts == outposts)
+        continue;
+      for (Loc outpost : enemyOutposts){
+        double tempDist = outpost.distance(consCenter);
+        if (tempDist < enemyDist){
+          enemyDist = tempDist;
+          centerTarget = outpost;
+        }
+      }
+    }
+    System.out.println("Moving to target " + centerTarget);
+
+    Loc north = board.nearestLand(new Loc(centerTarget.x,centerTarget.y + r));
+    Loc south = board.nearestLand(new Loc(centerTarget.x,centerTarget.y - r));
+    Loc east = board.nearestLand(new Loc(centerTarget.x + r,centerTarget.y));
+    Loc west = board.nearestLand(new Loc(centerTarget.x - r,centerTarget.y));
+
+    targets.add(north);
+    targets.add(south);
+    targets.add(east);
+    targets.add(west);
   }
 
 }
