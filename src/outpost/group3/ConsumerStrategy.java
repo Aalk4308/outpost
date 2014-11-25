a3d2e317b8841157bc2d86330fdf4ef7d7a60139
powerrmt <powerrmt@gmail.com>
2014-11-25 15:54:38 -0500

    ConsumerStrategy now asks for 4 outposts from the general in Player.java; modified ConsumerStrategy to work with updated infrastructure; if the formation gets broken up in ConsumerStrategy, revert back to BUILD state until it fixes itself; added the ASSIGN state to deal with cases where the whole formation is no longer available

--- src/outpost/group3/ConsumerStrategy.java
+++ src/outpost/group3/ConsumerStrategy.java
@@ -9,17 +9,14 @@ public class ConsumerStrategy extends outpost.group3.Strategy {
   private int r;
   private int size = 100;
   private int numSoldiers = 0;
-  enum State { BUILD, ATTACK };
-  static State state = State.BUILD;
-
-  //These will represent our current outpost locations
-  ArrayList<Loc> outposts = new ArrayList<Loc>();
+  enum State { ASSIGN, BUILD, ATTACK };
+  static State state = State.ASSIGN;
 
   //center of our consumer at the start
-  Loc consCenter = new Loc(); 
+  static Loc consCenter = new Loc(); 
 
-  //These represent the positions of the 4 outposts composing a consumer
-  Loc[] consumer = new Loc[4];
+  //These are the 4 outposts composing a consumer
+  Outpost[] consumer = new Outpost[4];
   final int NORTH = 0;
   final int EAST = 1;
   final int SOUTH = 2;
@@ -33,90 +30,121 @@ public class ConsumerStrategy extends outpost.group3.Strategy {
   }
 
   public void run(Board board, ArrayList<Outpost> outposts) {
-	  /*
-    ArrayList<Loc> targets = new ArrayList<Loc>();
-
-    outposts = board.ourOutposts();
-    int numOutposts = outposts.size();
-
-
-    Loc firstOutpost = new Loc(0,0);
-    double minDistance = 1000000000.0;
-
-    //Pick a location where we'll form the consumer that's close to our corner
-    for (int i = r; i < size - r; i++){
-      for (int j = r; j < size - r; j++){
-        if (!board.getCell(i,j).isWater()){
-          if(!board.getCell(i-r,j).isWater() &&
-              !board.getCell(i,j-r).isWater() &&
-              !board.getCell(i+r,j).isWater() &&
-              !board.getCell(i,j+r).isWater()){
-            Loc temp = new Loc(i,j);
-            if (firstOutpost.distance(temp) < minDistance){
-              minDistance = consCenter.distance(temp);
-              consCenter = temp;
-            }
-          }
-        }
-      }
-    } 
 
     if (outposts.size() >= 4){
+
+      //TODO: Strategically reassign the outposts that aren't assigned
+      //and add a new RELOCATE state
+      ArrayList<String> filledRoles = new ArrayList<String>();
+      for (Outpost outpost : outposts){
+        if (outpost.memory.containsKey("role"))
+          filledRoles.add((String)outpost.memory.get("role"));
+      }
+      if (filledRoles.size() < 4){
+        state = State.ASSIGN;
+        for (Outpost outpost : outposts){
+          if (outpost.memory.containsKey("role"))
+            filledRoles.remove((String)outpost.memory.get("role"));
+        }
+        for (Outpost outpost : outposts){
+          if(!outpost.memory.containsKey("role") && !filledRoles.isEmpty())
+            outpost.memory.put("role",filledRoles.remove(0));
+        }
+      }
+
       switch (state){
+
+        case ASSIGN:
+
+          int numOutposts = outposts.size();
+
+          Loc firstOutpost = new Loc(0,0);
+          double minDistance = 1000000000.0;
+
+          //Pick a location where we'll form the consumer that's close to our corner
+          for (int i = r; i < size - r; i++){
+            for (int j = r; j < size - r; j++){
+              if (!board.getCell(i,j).isWater()){
+                if(!board.getCell(i-r,j).isWater() &&
+                    !board.getCell(i,j-r).isWater() &&
+                    !board.getCell(i+r,j).isWater() &&
+                    !board.getCell(i,j+r).isWater()){
+                  Loc temp = new Loc(i,j);
+                  if (firstOutpost.distance(temp) < minDistance){
+                    minDistance = consCenter.distance(temp);
+                    consCenter = temp;
+                  }
+                }
+              }
+            }
+          } 
+
+          //Put first four outposts in first outpost
+          for (int i = 0; i < 4; i ++)
+            outposts.get(i).memory.put("formation",1);
+
+          //Assign each outpost its position
+          outposts.get(NORTH).memory.put("role","north");
+          outposts.get(SOUTH).memory.put("role","south");
+          outposts.get(WEST).memory.put("role","west");
+          outposts.get(EAST).memory.put("role","east");
+
+          //Set state to BUILD and fall through
+          state = State.BUILD;
+
         case BUILD: 
-          
-          int numInPosition = 0;
-
-          consumer[NORTH] = new Loc(consCenter.x,consCenter.y + r);
-          consumer[SOUTH] = new Loc(consCenter.x,consCenter.y - r);
-          consumer[EAST] = new Loc(consCenter.x + r,consCenter.y);
-          consumer[WEST] = new Loc(consCenter.x - r,consCenter.y);
-
-          //Are we in formation?
-          for (Loc outpost : outposts)
-            for (int i = 0; i < 4; i++)
-              if (consumer[i].equals(outpost))
-                numInPosition++;
-          System.out.println(numInPosition);
-          //We have fully formed a consumer
-          if(numInPosition >= 4){
-            state = State.ATTACK;
-            attackMove(targets,board);
-          }
-          else{
-            for (int i = 0; i < 4; i++)
-              targets.add(consumer[i]);
-          }
-
+          buildMove(outposts,board);
           break;
 
         case ATTACK: 
-          attackMove(targets,board);
+          attackMove(outposts,board);
           break;
 
-      }
-
-      for (int i = 4; i < outposts.size(); i++){
-        targets.add(outposts.get(i));
       }
 
     }
     else{
-      double sideLength = Math.min(65, Math.sqrt(Math.pow(2*board.r*numOutposts * 1, 2) / 2));
-
-      for (int outpostId = 0; outpostId < numOutposts; outpostId++) {
-        int xTarget = (int) Math.round((outpostId + 1) * (sideLength / (numOutposts + 1)));
-        int yTarget = (int) Math.round((numOutposts - outpostId) * (sideLength / (numOutposts + 1)));
-
-        targets.add(board.nearestLand(new Loc(xTarget, yTarget)));
-      }
+      for (Outpost outpost : outposts)
+        outpost.setStrategy(null);
     }
-    return targets;
-    */
+  }
+
+  //Given the center of a formation, assign the consumer outposts to their associated positions
+  private void setFormationLocs(ArrayList<Outpost> outposts, Loc formationCenter, Board board){
+    for (Outpost outpost : outposts){
+      if(outpost.memory.get("role").equals("north"))
+        outpost.setTargetLoc(board.nearestLand(new Loc(formationCenter.x,formationCenter.y + r)));
+      else if(outpost.memory.get("role").equals("south"))
+        outpost.setTargetLoc(board.nearestLand(new Loc(formationCenter.x,formationCenter.y - r)));
+      else if(outpost.memory.get("role").equals("east"))
+        outpost.setTargetLoc(board.nearestLand(new Loc(formationCenter.x + r,formationCenter.y)));
+      else if(outpost.memory.get("role").equals("west"))
+        outpost.setTargetLoc(board.nearestLand(new Loc(formationCenter.x - r,formationCenter.y)));
+    }
+  }
+
+  private void buildMove(ArrayList<Outpost> outposts, Board board){
+    int numInPosition = 0;
+
+    setFormationLocs(outposts,consCenter,board);
+
+    //Are we in formation?
+    for (Outpost outpost : outposts){
+      if (outpost.getCurrentLoc().equals(outpost.getTargetLoc()))
+        numInPosition++;
+    }
+
+    //We have fully formed a consumer
+    if(numInPosition >= 4){
+      state = State.ATTACK;
+      for (Outpost outpost : outposts)
+        outpost.memory.put("expectedSpot",outpost.getExpectedLoc());
+      attackMove(outposts,board);
+    }
   }
 
   //Set the next move for our consumer formation to attack the closest enemy
-  private void attackMove(ArrayList<Loc> targets, Board board){
+  private void attackMove(ArrayList<Outpost> outposts, Board board){
 
     //TODO: Make it so I create a new consumer every 4 outposts
 
@@ -126,94 +154,95 @@ public class ConsumerStrategy extends outpost.group3.Strategy {
 
     Loc centerTarget = new Loc();
     ArrayList<Loc> enemyOutposts = new ArrayList<Loc>();
+    ArrayList<Outpost> outOfFormation = new ArrayList<Outpost>();
 
-    //Find closest enemy
-    for (int i = 0; i < 4; i ++){
-      enemyOutposts = board.theirOutposts(i);
-      if (enemyOutposts == outposts)
-        continue;
-      for (Loc outpost : enemyOutposts){
-        double tempDist = outpost.distance(consCenter);
-        if (tempDist < enemyDist){
-          enemyDist = tempDist;
-          centerTarget = outpost;
+    for (Outpost outpost : outposts){
+      Loc spot = (Loc)outpost.memory.get("expectedSpot");
+      Loc realSpot = outpost.getExpectedLoc();
+      //System.out.println("Oupost " + outpost.memory.get("role") + " should be at " + spot + " but is at " + realSpot);
+      if(spot.x != realSpot.x && spot.y != realSpot.y){
+        outOfFormation.add(outpost);
+      }
+    }
+
+    if (!outOfFormation.isEmpty()){
+      //System.out.println("Out of formation is not empty");
+      //If the current state of the formation is not what was expected after the last move
+      //keep all correct soldiers in their spots and move all others so they're on the correct
+      //axis of their formation spot
+      state = State.BUILD;
+      buildMove(outposts,board);
+    }
+    else{
+      //Find closest enemy
+      for (int i = 0; i < 4; i ++){
+        enemyOutposts = board.theirOutposts(i);
+        if (enemyOutposts == board.ourOutposts())
+          continue;
+        for (Loc outpost : enemyOutposts){
+          double tempDist = outpost.distance(consCenter);
+          if (tempDist < enemyDist){
+            enemyDist = tempDist;
+            centerTarget = outpost;
+          }
         }
       }
-    }
 
-    //Ideal target positions for our formation
-    Loc[] idealTargets = formationAround(centerTarget);
-
-    //Determine which member of our ideal formation is closest to the target
-    Loc closest = new Loc();
-
-    int soldier = 0; 
-    int secondarySold = 0; 
-    enemyDist = 100;
-    for (int i = 0; i < 4; i++){
-      System.out.println("Distance for outpost " + i + " is " + outposts.get(i).distance(centerTarget));
-      if (outposts.get(i).distance(centerTarget) < enemyDist){
-        System.out.println("Outpost " + i + " is closer at location " + outposts.get(i));
-        closest = outposts.get(i);
-        enemyDist = outposts.get(i).distance(centerTarget);
-        secondarySold = soldier;
-        soldier = i;
+      //Determine which member of our ideal formation is closest to the target
+      String closest = "";
+      String secondClosest = ""; 
+      enemyDist = 100;
+      for (Outpost outpost : outposts){
+        if (outpost.getCurrentLoc().distance(centerTarget) < enemyDist){
+          secondClosest = closest;
+          closest = (String) outpost.memory.get("role");
+          enemyDist = outpost.getCurrentLoc().distance(centerTarget);
+        }
       }
+
+      Loc nextMoveTarget = new Loc();
+      //TODO: If the soldier direction would put us on water, use secondary soldier direction
+
+      if(closest.equals("north")){
+        nextMoveTarget = board.nearestLand(new Loc(centerTarget.x,centerTarget.y + r));
+        consCenter = new Loc(consCenter.x, consCenter.y + 1);
+        for (Outpost outpost : outposts){
+          Loc newspot = new Loc(outpost.getCurrentLoc());
+          newspot.y++;
+          outpost.memory.put("expectedSpot",newspot);
+        }
+      }
+      else if(closest.equals("south")){
+        nextMoveTarget = board.nearestLand(new Loc(centerTarget.x,centerTarget.y - r));
+        consCenter = new Loc(consCenter.x, consCenter.y - 1);
+        for (Outpost outpost : outposts){
+          Loc newspot = new Loc(outpost.getCurrentLoc());
+          newspot.y--;
+          outpost.memory.put("expectedSpot",newspot);
+        }
+      }
+      else if(closest.equals("east")){
+        nextMoveTarget = board.nearestLand(new Loc(centerTarget.x + r,centerTarget.y));
+        consCenter = new Loc(consCenter.x + 1, consCenter.y);
+        for (Outpost outpost : outposts){
+          Loc newspot = new Loc(outpost.getCurrentLoc());
+          newspot.x++;
+          outpost.memory.put("expectedSpot",newspot);
+        }
+      }
+      else if(closest.equals("west")){
+        nextMoveTarget = board.nearestLand(new Loc(centerTarget.x - r,centerTarget.y));
+        consCenter = new Loc(consCenter.x - 1, consCenter.y);
+        for (Outpost outpost : outposts){
+          Loc newspot = new Loc(outpost.getCurrentLoc());
+          newspot.x--;
+          outpost.memory.put("expectedSpot",newspot);
+        }
+      }
+
+      setFormationLocs(outposts,consCenter,board);
     }
 
-
-    Loc nextMove = new Loc();
-    //TODO: If the soldier direction would put us on water, use secondary soldier direction
-    switch(soldier){
-      case NORTH: nextMove = board.nearestLand(new Loc(centerTarget.x,centerTarget.y + r));
-                  consCenter = new Loc(consCenter.x, consCenter.y + 1);
-                  System.out.println("We're moving north");
-                  for (int i = 0; i < 4; i ++){
-                    consumer[i] = board.nearestLand(new Loc(outposts.get(i).x,outposts.get(i).y + 1));
-                  }
-                  break;
-      case SOUTH: nextMove = board.nearestLand(new Loc(centerTarget.x,centerTarget.y - r));
-                  consCenter = new Loc(consCenter.x, consCenter.y - 1);
-                  for (int i = 0; i < 4; i ++){
-                    consumer[i] = board.nearestLand(new Loc(outposts.get(i).x,outposts.get(i).y - 1));
-                  }
-                  System.out.println("We're moving south");
-                  break;
-      case EAST: nextMove = board.nearestLand(new Loc(centerTarget.x + r,centerTarget.y));
-                  consCenter = new Loc(consCenter.x + 1, consCenter.y );
-                  for (int i = 0; i < 4; i ++){
-                    consumer[i] = board.nearestLand(new Loc(outposts.get(i).x + 1,outposts.get(i).y));
-                  }
-                  System.out.println("We're moving east");
-                  break;
-      case WEST: nextMove = board.nearestLand(new Loc(centerTarget.x - r,centerTarget.y));
-                  consCenter = new Loc(consCenter.x - 1, consCenter.y);
-                  for (int i = 0; i < 4; i ++){
-                    consumer[i] = board.nearestLand(new Loc(outposts.get(i).x - 1,outposts.get(i).y));
-                  }
-                  System.out.println("We're moving west");
-                  break;
-    }
-
-    ArrayList<Loc> path = board.findPath(closest,nextMove);
-
-
-    System.out.println("New positions are ");
-
-    for (int i = 0; i < 4; i ++){
-    System.out.println(consumer[i]);
-      targets.add(consumer[i]);
-    }
-  }
-
-  private Loc[] formationAround(Loc t){
-    Loc[] spots = new Loc[4];
-    spots[NORTH] = new Loc(t.x,t.y + r);
-    spots[SOUTH] = new Loc(t.x,t.y - r);
-    spots[EAST] = new Loc(t.x + r,t.y);
-    spots[WEST] = new Loc(t.x - r,t.y);
-
-    return spots;
   }
 
 }
