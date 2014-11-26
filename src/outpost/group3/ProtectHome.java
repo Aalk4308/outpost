f57030e64b2952302a1f848ea30e3d7bcfb66e8b
msd2178 <msd2178@columbia.edu>
2014-11-25 20:33:41 -0500

    modified target locations-independent of r

--- src/outpost/group3/ProtectHome.java
+++ src/outpost/group3/ProtectHome.java
@@ -8,7 +8,7 @@ public class ProtectHome extends outpost.group3.Strategy{
 	
 	public void run(Board board, ArrayList<Outpost> outposts) {
 				
-			Loc loc0 = new Loc((int)board.r,(int)board.r);
+			Loc loc0 = new Loc(0,0);
 			Cell cell0 = board.getCell(loc0);
 			if (cell0.isLand())
 				outposts.get(0).setTargetLoc(loc0);
@@ -16,14 +16,14 @@ public class ProtectHome extends outpost.group3.Strategy{
 				outposts.get(0).setTargetLoc(board.nearestLand(loc0));
 			
 			
-			Loc loc1 = new Loc((int)board.r*2,0);
+			Loc loc1 = new Loc(1,0);
 			Cell cell1 = board.getCell(loc1);
 			if (cell1.isLand())
 				outposts.get(1).setTargetLoc(loc1);
 			else
 				outposts.get(1).setTargetLoc(board.nearestLand(loc1));
 			
-			Loc loc2 = new Loc(0,(int)board.r*2);
+			Loc loc2 = new Loc(0,1);
 			Cell cell2 = board.getCell(loc2);
 			if (cell2.isLand())
 				outposts.get(2).setTargetLoc(loc2);
